7c9164f71de7c68364faf4ec127c9531c127ceeb
Shalinij14 <shalinijs235@gmail.com>
2026-04-14 23:35:50 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class App {
 
